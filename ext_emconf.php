f16fa3a574712e1af4f7d7b22542e1355d80184f
Sven Jürgens <SvenJuergens@users.noreply.github.com>
2024-01-23 21:14:32 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-//#######################################################################
-// Extension Manager/Repository config file for ext "ni_accordion".
-//
-// Auto generated 15-10-2013 13:29
-//
-// Manual updates:
-// Only the data in the array - everything else is removed by next
-// writing. "version" and "dependencies" must not be touched!
-//#######################################################################
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'minihelper',
     'description' => 'Minihelper, some function missing in TYPO3 12',
